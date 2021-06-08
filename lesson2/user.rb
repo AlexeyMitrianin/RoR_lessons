e04e02411980c4d0850fda1de6b8be3abb74bbfa
Aleksey_Mitryanin <aleksey-mitryanin@yandex.ru>
2021-06-08 16:24:54 +0300

    Добавление класса User в файл user.rb
    Обновление файла user_update.rb(присоединение класа User и вывод в файл  через file.write

--- lesson2/user.rb
+++ lesson2/user.rb
@@ -1,33 +1,31 @@
-  class User
-    def surname
-     'Митрянин'
-    end
-
-    def name_user
-      'Алексей'
-    end
-
-    def patronymic
-     'Игоревич'
-    end
-
-    def date_of_birth
-      '26.08.1988'
-    end
-
-    def reg_address
-      'Рязанская область, г.Рязань, ул.Чапаева, д.47, кв.129'
-    end
-
-    def res_address
-      'Совпадает с адресом регистрации'
-    end
+class User
+  def surname
+    print 'Введите вашу фамилию:'
+    surname = gets.chomp
   end
 
-  user = User.new
-  file = File.new("user.txt", "w")
-    file.puts "Гражданин #{user.surname} #{user.name_user} #{user.patronymic}"
-    file.puts 'Родился:' + user.date_of_birth
-    file.puts 'Зарегистрирован по адресу:' + user.reg_address
-    file.puts 'Проживает по адресу:' + user.res_address
-  file.close
+  def name_user
+    print 'Введите ваше имя:'
+    name_user = gets.chomp
+  end
+
+  def patronymic
+    print 'Введите ваше отчество:'
+    patronymic = gets.chomp
+  end
+
+  def date_of_birth
+    print 'Введите вашу дату роджения:'
+    date_of_birth = gets.chomp
+  end
+
+  def reg_address
+    print 'Введите ваш адрес регистрации:'
+    reg_address = gets.chomp
+  end
+
+  def res_address
+    print 'Введите ваш адрес проживания:'
+    reg_address = gets.chomp
+  end
+end
