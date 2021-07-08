0ed4924c1e4a81fad20a435d7976f1588c7b884b
Aleksey_Mitryanin <aleksey-mitryanin@yandex.ru>
2021-07-08 08:47:07 +0300

    update

--- Part2/lesson2/colors_new.rb
+++ Part2/lesson2/colors_new.rb
@@ -8,7 +8,6 @@ colors.map do |_color|
   print 'Введите дополнительный цвет (для выхода наберите stop): '
   name_color = gets.chomp
   break if  (name_color.eql? 'stop') || (name_color.eql? 'Stop') || (name_color.eql? 'STOP')
-
   colors << name_color
 end
 p colors.uniq.sort
